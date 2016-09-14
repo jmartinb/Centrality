6f7f17ed9db95ad81246cea6a811750d107ef818
jmartinb <javier.martin.blanco@cern.ch>
2016-09-14 16:56:41 +0200

    Modification of InputTag on pACentralityBin config

--- pPbReco2016/pACentralityBin_cfi.py
+++ pPbReco2016/pACentralityBin_cfi.py
@@ -1,21 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
 centralityBin_HFPlusFwd = cms.EDProducer('CentralityBinProducer',
-    Centrality = cms.InputTag("hiCentrality"),
+    Centrality = cms.InputTag("pACentrality"),
     centralityVariable = cms.string("HFtowersPlusTrunc"),
     nonDefaultGlauberModel = cms.string(""),
     pPbRunFlip = cms.uint32(99999999),
 )
 
 centralityBin_HFMinusFwd = cms.EDProducer('CentralityBinProducer',
-    Centrality = cms.InputTag("hiCentrality"),
+    Centrality = cms.InputTag("pACentrality"),
     centralityVariable = cms.string("HFtowersMinusTrunc"),
     nonDefaultGlauberModel = cms.string(""),
     pPbRunFlip = cms.uint32(99999999),
 )
 
 centralityBin_HFSumFwd = cms.EDProducer('CentralityBinProducer',
-    Centrality = cms.InputTag("hiCentrality"),
+    Centrality = cms.InputTag("pACentrality"),
     centralityVariable = cms.string("HFtowers"),
     nonDefaultGlauberModel = cms.string(""),
     pPbRunFlip = cms.uint32(99999999),
